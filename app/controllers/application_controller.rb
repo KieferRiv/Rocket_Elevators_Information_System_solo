4ce6b922de83c038ee54870e86bcb3f2f11b3311
KieferRiv <kieferriv@DESKTOP-4NH9J8P.localdomain>
2020-11-01 12:08:43 -0500

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :firstname, :lastname, :email, :password, :current_password])
